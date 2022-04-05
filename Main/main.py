3f02d429e982752b2e62e66b7e05daa9b9db6206
SergioLJDC <sergio.licanqueo@jdc.cl>
2022-04-05 12:36:28 -0400

    05-04

--- Main/main.py
+++ Main/main.py
@@ -18,7 +18,7 @@ import pandas as pd
 #EXCEL
 ipath = 'main\Seguimiento legislativo.xlsx'
 df = pd.read_excel(ipath)
-
+df["Flag"] = pd.NaT
 
 def ListDeElementosTable(url):
     pagList = [1]
@@ -58,40 +58,44 @@ def selenium(index,url,i):
     PATH = "C:\Program Files (x86)\msedgedriver.exe"
     driver = webdriver.Edge(PATH)
    
-
     try:
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html5lib')
+        driver.get(url)
         table = soup.find('div', attrs={'class': 'paginacion aleft'})
-
-        print("try",url,index)
-
+        print("LOG : TRY")
+        print(driver.get(url))
         pagJurista = table.find("span")
         pagJurista = pagJurista.get_text()
+        print(index is not 1)
+        time.sleep(5)
+        
+        
+        if(index is not 1):
+                paginacion_aleft = driver.find_element(By.ID, "ContentPlaceHolder1_ContentPlaceHolder1_ContentPlaceHolder1_pager_rptPager_page_{}".format(index))
+                paginacion_aleft.click()
+                print("IF")
 
-        
-        driver.get(url)
-        if(index != "1"):
-            paginacion_aleft = driver.find_element(By.ID, "ContentPlaceHolder1_ContentPlaceHolder1_ContentPlaceHolder1_pager_rptPager_page_{}".format(index))
-            paginacion_aleft.click()
-            print("IF")
-        
 
         soup = BeautifulSoup(driver.page_source)
+        print(url)
+        print(soup)
         soup_table = soup.find_all("table")[-1]
         soup_table = soup.find("table")
-        time.sleep(3)
         tables = pd.read_html(str(soup_table))
         dataf = tables[-1]
         # FECHA EXCEL = FECHA WEB
         df.iloc[i,9] =  dataf.iloc[len(dataf)-1][0]
         # ULTIMO MOVIMIENTO EXCEL = ULTIMO MOVIMIENTO WEB "Sub-etapa"
         df.iloc[i,7] = dataf.iloc[len(dataf)-1][3] 
+        print(dataf.iloc[len(dataf)-1][3])
         #TRAMITE EXCEL = ETAPA WEB
         df.iloc[i,5] = dataf.iloc[len(dataf)-1][2] 
+        #FLAG
+        df.iloc[i,11] = "Update"
 
-        
     except:
+        df.iloc[i,11] = "Error"
         pass
     driver.quit() 
 
@@ -109,8 +113,8 @@ for i in df.index:
     fechaUltimoMovimiento = df.iloc[i,9]
     url = df.iloc[i,10]
     index = (ListDeElementosTable(url))
-    print("INDEX :", type(index))
-    print("URL WHILE:  ",url  , type(url))
+   # print("INDEX :", type(index))
+   # print("URL WHILE:  ",url  , type(url))
     selenium(str(index),url,i)
     i = i +1 
 
